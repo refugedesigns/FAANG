eeb3cc59e7772830f7ada4186d6a01592c21c3e9
Antwi Erasmus <70457144+refugedesigns@users.noreply.github.com>
2022-05-26 04:47:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// DATA TYPES
+
+/* 
+-------NUMBERS---------
+1. Used for any type of calculations
+2. Numbers with no decimals are called => INTEGERS eg. 3, -5, 8, -20
+3. Numbers with decimals are called => FLOATS eg. 23.422, 4.5
+*/
+
+/* 
+----------STRINGS-----------
+1. Used when collecting information from the user or output a message to the user
+2. Strings are written using 
+a. Single qoutes eg 'Hi'
+b. Double qoutes eg "Hi"
+c. Back ticks qoutes eg `Hi`
+*/
+
